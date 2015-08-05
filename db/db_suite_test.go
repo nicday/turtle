4ba42a761f1199e8390ea3c144bb46f9d21ef1ce
Nic Day <nic.day@me.com>
2015-08-06 11:35:35 +1200

    Set short BackoffTimeout for testing

--- db/db_suite_test.go
+++ db/db_suite_test.go
@@ -5,9 +5,15 @@ import (
 	. "github.com/onsi/gomega"
 
 	"testing"
+	"time"
 )
 
 func TestDb(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Db Suite")
 }
+
+var _ = BeforeSuite(func() {
+	// Reduce the backoff timeout to help avoid long running tests
+	BackoffTimeout = time.Duration(1) * time.Second
+})
