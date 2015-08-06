2c472182c397223c60757beabc4fd32f9196eadf
Nic Day <nic.day@me.com>
2015-08-06 17:43:30 +1200

    Add comments

--- migration/sort.go
+++ migration/sort.go
@@ -2,16 +2,19 @@ package migration
 
 import "sort"
 
+// sortedMigrations implements sort.Sort
 type sortedMigrations struct {
 	migrations map[string]*Migration
 	direction  string
 	sorted     []*Migration
 }
 
+// Len satisfies the sort.Sort interface.
 func (s *sortedMigrations) Len() int {
 	return len(s.migrations)
 }
 
+// Less satisfies the sort.Sort interface.
 func (s *sortedMigrations) Less(i, j int) bool {
 	if s.direction == "desc" {
 		return s.migrations[s.sorted[i].ID].ID > s.migrations[s.sorted[j].ID].ID
@@ -19,10 +22,12 @@ func (s *sortedMigrations) Less(i, j int) bool {
 	return s.migrations[s.sorted[i].ID].ID < s.migrations[s.sorted[j].ID].ID
 }
 
+// Swap satisfies the sort.Sort interface.
 func (s *sortedMigrations) Swap(i, j int) {
 	s.sorted[i], s.sorted[j] = s.sorted[j], s.sorted[i]
 }
 
+// SortMigrations will sort migrations in either ascending or descending order based on their ID.
 func SortMigrations(migrations map[string]*Migration, direction string) []*Migration {
 	s := sortedMigrations{}
 	s.migrations = migrations
